490378a5474e971e9b79024ffc303fb538bc3576
ShiviDev <12shivi.ani@gmail.com>
2023-06-19 15:14:58 +0530

    Added camera upload feature and comments

--- app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,17 +32,25 @@ import java.util.Locale
 
 class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
+    /*Calendar initialisation*/
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    /*View declarations*/
     private lateinit var etdate: EditText
     private lateinit var tvaddImage : TextView
     private lateinit var ivPlaceImage : ImageView
+
+    /*Image initialisations*/
     private lateinit var galleryImageResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var cameraImageResultLauncher: ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
 
+        /*Initialise Views*/
         etdate = findViewById<EditText>(R.id.et_date)
         tvaddImage=findViewById<TextView>(R.id.tv_add_image)
         ivPlaceImage=findViewById(R.id.iv_place_image)
@@ -54,6 +63,7 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
+        //calendar popup listener
         dateSetListener =
             DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                 cal.set(Calendar.YEAR, year)
@@ -62,61 +72,103 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
                 updateDateInView()
             }
+
+        //view listeners
             etdate.setOnClickListener(this)
             tvaddImage.setOnClickListener(this)
 
+        //call for updating image from gallery
         registerOnActivityForGalleryResult()
+        registerOnActivityForCameraResult()
     }
+    //end of OnCreate
+
+    //listener handler
     override fun onClick(v: View?) {
         when (v!!.id) {
+            //setting date text et_date
             R.id.et_date -> {
                 DatePickerDialog(
                     this@AddHappyPlace,
                     dateSetListener,
-                    cal.get(Calendar.YEAR), // Here the cal instance is created globally and used everywhere in the class where it is required.
+                    cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+
+            //setting text view to act like a button
             R.id.tv_add_image->{
+                //alret pops up asking to select an action
                 val pictureDialog= AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select Action")
+                //options to select
                 val pictureDialogItems= arrayOf("Select photo from Gallery","Capture photo from Camera")
                 pictureDialog.setItems(pictureDialogItems){
                     _,which->
                     when(which){
-                        0->choosePhotoFromGallery()
-                        1->Toast.makeText(this@AddHappyPlace,"opening camera",Toast.LENGTH_SHORT).show()
+                        0->choosePhotoFromGallery()//callback function for gallery
+                        1->takePhotoFromCamera()
                     }
                 }
                 pictureDialog.show()
             }
         }
     }
+    //end of handler
+
+    //date formatter
     private fun updateDateInView(){
         val myFormat="dd.MM.yyyy"
         val sdf= SimpleDateFormat(myFormat, Locale.getDefault())
         etdate.setText(sdf.format(cal.time).toString())
     }
+    //end of date formatter
+
+    //choose photo from gallery
     private fun choosePhotoFromGallery(){
+        //Use of dexter
         Dexter.withContext(this).withPermissions(
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA).withListener(object: MultiplePermissionsListener {
-            override fun onPermissionsChecked(report: MultiplePermissionsReport?)
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?)//permission granted
             {
                 if(report!!.areAllPermissionsGranted()){
                 Toast.makeText(this@AddHappyPlace,"Storage READ/WRITE permissions are granted",Toast.LENGTH_SHORT).show()
-                    val galleryIntent=Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    val galleryIntent=Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)//call the gallery app
                     galleryImageResultLauncher.launch(galleryIntent)
                 }
             }
-            override fun onPermissionRationaleShouldBeShown(permissions:MutableList<PermissionRequest>, token: PermissionToken)
+            override fun onPermissionRationaleShouldBeShown(permissions:MutableList<PermissionRequest>, token: PermissionToken)//permission denied
             {
                 showRationalDialogForPermissions()
             }
         }).onSameThread().check()
     }
+    //end of choosePhotoFromGallery
+
+    //click photo throught camera
+    private fun takePhotoFromCamera() {
+        Dexter.withContext(this).withPermissions(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+        ).withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    cameraImageResultLauncher.launch(cameraIntent)
+                }
+            }
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+    //end of takePhotoFromCamera
+
+    //start of function to direct user to Settings page
     private fun showRationalDialogForPermissions(){
         AlertDialog.Builder(this@AddHappyPlace).setMessage("Allow permissions from settings to contuinue").setPositiveButton("GO TO SETTINGS")
         {
@@ -133,7 +185,9 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
             dialog.dismiss()
         }.show()
     }
+    //end of showRotationalDialogForPermissions
 
+    //after you get a result from gallery
     private fun registerOnActivityForGalleryResult(){
         galleryImageResultLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             result->
@@ -155,4 +209,21 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
+//end of registerOnActivityForGalleryResult
+
+    private fun registerOnActivityForCameraResult(){
+        cameraImageResultLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+
+                val thumbNail : Bitmap = data!!.extras?.get("data") as Bitmap
+                ivPlaceImage.setImageBitmap(thumbNail)
+            }
+        }
+    }
+
+
+
+
 }
